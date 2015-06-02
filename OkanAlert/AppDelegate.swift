f2e629846de7642d47f73bd20b4ccaaec1b362d4
youcune <you@nakanishi.email>
2015-06-02 15:18:33 +0900

    Initial Commit

---
+++ OkanAlert/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  OkanAlert
+//
+//  Created by Yu Nakanishi on 2015/06/02.
+//  Copyright (c) 2015年 Yu Nakanishi. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ OkanAlert/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  OkanAlert
+//
+//  Created by Yu Nakanishi on 2015/06/02.
+//  Copyright (c) 2015年 Yu Nakanishi. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
